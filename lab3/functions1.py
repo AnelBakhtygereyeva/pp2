e1e8c1fe5c0cb7e7a9007725458e05b52feae366
AnelBakhtygereyeva <156872354+AnelBakhtygereyeva@users.noreply.github.com>
2024-01-24 12:48:29 +0600

    Update functions1.py

--- lab3/functions1.py
+++ lab3/functions1.py
@@ -1,6 +1,15 @@
+#1
 def myFunction(grams):
     ounces = 28.3495231 * float(grams)
     print(ounces)
 
 grams = input()
 myFunction(grams)
+
+#2
+def FahrenheitToCentigrade(fahrenheit):
+    centigrade = (5 / 9) * (float(fahrenheit) - 32)
+    print(centigrade)
+
+fahrenheit = input()
+FahrenheitToCentigrade(fahrenheit)
